a520d7d17f2bf9a6050287fa58e2206ff9ec72b4
nickma101 <nimima95@gmail.com>
2021-06-17 07:40:11 +0200

    minor changes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
 from app.selection import newsselector
 
-@app.route('/', methods= ['GET'])
+#Homepage that displays news articles
+@app.route('/', methods= ['GET', 'POST'])
+@app.route('/homepage', methods= ['GET', 'POST'])
 def select_news():
 	return "This will become a newsapp!"
+
+#User registration, log-in and log-out
+@app.route('/register', methods=['GET', 'POST'])
+	
+@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/logout')
+
+#User profile with statistics -> to be added at a later stage
+@app.route('/profile')
--- app/selection.py
+++ app/selection.py
@@ -5,18 +5,18 @@
 #How many articles should be selected?
 how_many = 10
 #which topics should be selected
-topics = ?
+topics = "?"
 #Among how many of the most recent articles should this selection be made?
 among_sample = 100
 #What is the selection logic (see recommender file)?
-recommender = random_selection
+#recommender = random_selection
 
 class newsselector():
 	def __init__(self):
-        self.how_many = how_many
-        self.topics = topics
-        self.among_sample = among_sample
-        self.recommender = recommender
+		self.how_many = how_many
+		self.topics = topics
+		self.among_sample = among_sample
+#		self.recommender = recommender
 
 #Function that selects n articles according to the previously defined input
 	def select_articles(how_many, topics, among_sample, recommender):
