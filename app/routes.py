073d974e1ffc80f5018440b31f383ac938e71fe4
nickma101 <nimima95@gmail.com>
2021-06-18 09:03:56 +0200

    minor fixes

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,3 @@ from app.selection import newsselector
 @app.route('/homepage', methods= ['GET', 'POST'])
 def select_news():
 	return "This will become a newsapp!"
-
-#User registration, log-in and log-out
-@app.route('/register', methods=['GET', 'POST'])
-	
-@app.route('/login', methods = ['GET', 'POST'])
-
-@app.route('/logout')
-
-#User profile with statistics -> to be added at a later stage
-@app.route('/profile')
