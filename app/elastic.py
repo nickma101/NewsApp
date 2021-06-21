1f4d3150f39d3465f7c07e6481ba733098fd4fd5
nickma101 <nimima95@gmail.com>
2021-06-21 06:28:42 +0200

    remove elastic file content - currently unneccessary

--- app/elastic.py
+++ app/elastic.py
@@ -1,11 +1,1 @@
-from elasticsearch import Elasticsearch
-from amcatclient import AmcatAPI
 
-es = Elasticsearch()
-
-amcat = AmcatAPI("https://vu.amcat.nl", "nickmattis", "Nimima&95")
-
-es.indices.create("articles")
-
-for article in amcat.get_articles(project=69, articleset=2564, start_date="2021-03-09", columns=("date", "title", "publisher", "url", "text", "author", "section")):
-    es.create(index="article", id=article['id'], body=article)
