01819d49381c0afefa1ae02e2ba2076eacf2a2e0
nickma101 <nimima95@gmail.com>
2021-07-13 17:12:54 +0200

    requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask
-from flask_login import LoginManager
-from flask_migrate import Migrate
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 db = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
-login = LoginManager(app)
 
 from app import routes, elastic
 
