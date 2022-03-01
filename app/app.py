c3ab9e97ef3f8be4a0216f1638b58ac9bdf2e82c
nickma101 <nimima95@gmail.com>
2022-03-01 15:59:41 +0100

    magic

--- app/app.py
+++ app/app.py
@@ -1,21 +1,19 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api, reqparse
-from recommender import recommendations
-
+import recommender
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = "youwillneverguess"
-api = Api(app)
 
-class Recommendations(Resource):
-    recommendations = recommendations
-    def get(self):
-        return recommendations
 
-#api endpoints
-#api.add_resource(Article, '/articles/<string:id>')
-api.add_resource(recommendations, '/recommendations')
+@app.route("/recommendations", methods=["GET"])
+def get_recommendations():
+    experiment_id=request.args.get('experiment_id')
+    user_id=request.args.get('user_id')
+    if not experiment_id:
+        raise Exception("No exepriment id given")
+    return jsonify(recommender.get_articles(experiment_id, user_id))
+
 
 
 if __name__ == '__main__':
--- app/recommender.py
+++ app/recommender.py
@@ -7,7 +7,8 @@ Handles the articles that are displayed to users
 """
 
 from amcatclient import AmcatAPI
-from experimental_settings import experimental_parameters
+from experimental_settings import get_settings
+import json
 
 """
 1) Retriving articles (the stuff below will go into the config file)
@@ -15,23 +16,17 @@ from experimental_settings import experimental_parameters
 
 amcat = AmcatAPI("https://vu.amcat.nl", "NickMattis")
 
-def retrieve_articles(parameters):
-    parameters = parameters
+def get_articles(experiment_id, user_id):
+    settings = get_settings(experiment_id)
     articles = []
-    for article in amcat.get_articles(project = parameters['project'],
-    articleset=parameters['articleset'],
-    start_date=parameters['start_date'],
-    columns=parameters['columns']):
+    for article in amcat.get_articles(project = settings['project'],
+            articleset=settings['articleset'],
+            start_date=settings['start_date'],
+            columns=settings['columns']):
         articles.append(article)
     return articles
 
-#or article in amcat.get_articles(project=69, articleset=2564, start_date="2021-03-09", columns=("date", "title", "publisher", "url", "text", "author", "section")):
 
 """
 2) Retrieve user data and experimental settings
 """
-
-"""
-Make recommendations
-"""
-recommendations = retrieve_articles(experimental_parameters)
