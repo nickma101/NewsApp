c2a93923d18c0370f51c80085e006107c91c166e
nickma101 <nimima95@gmail.com>
2022-03-02 18:25:07 +0100

    allow cross-domain access

--- app/app.py
+++ app/app.py
@@ -1,19 +1,23 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import recommender
 
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = "youwillneverguess"
+
+#to allow cross-domain access during development stage
+cors = CORS(app)
+app.config['CORS_Headers'] = 'Content-Type'
 
 
 @app.route("/recommendations", methods=["GET"])
 def get_recommendations():
-    experiment_id=request.args.get('experiment_id')
-    user_id=request.args.get('user_id')
+    experiment_id = request.args.get('experiment_id')
+    user_id = request.args.get('user_id')
     if not experiment_id:
-        raise Exception("No exepriment id given")
+        raise Exception("No experiment id given")
     return jsonify(recommender.get_articles(experiment_id, user_id))
-
 
 
 if __name__ == '__main__':
--- app/experimental_settings.py
+++ app/experimental_settings.py
@@ -1,7 +1,11 @@
 """
 Define experimental settings as json dictionary (article retrieval & display)
 """
-experiment1 = {"no_articles_displayed":2,"project":9, "articleset":2564, "start_date": "2021-03-16", "columns": ("date", "title", "publisher", "url", "text", "author", "section")}
+experiment1 = {"no_articles_displayed":2,
+               "project":9,
+               "articleset":2564,
+               "start_date": "2021-03-16",
+               "columns": ("date", "title", "publisher", "url", "text", "author", "section")}
 
 def get_settings(name: str):
     return globals()[name]
