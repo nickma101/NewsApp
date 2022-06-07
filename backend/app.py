d388cbdca347a4bcdb08f10b46d014a529fa584f
nickma101 <nimima95@gmail.com>
2022-06-07 15:10:23 +0200

    adding user db

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 import recommender
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = "youwillneverguess"
 
@@ -20,6 +24,11 @@ def get_recommendations():
         raise Exception("No experiment id given")
     return jsonify(recommender.get_articles(experiment_id, user_id))
 
+
+@app.route("/users", methods=["GET", "PUT"])
+def test():
+    return "test"
+#def add_user():
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
---
+++ backend/database.py
@@ -0,0 +1,30 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    selected_news = db.relationship('News_sel', backref = 'user', lazy = 'dynamic')
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.id)
+
+'''
+DB for all articles that were selected (once a user selects an article it is added)
+'''
+class NewsSelected(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(500))
+    starttime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    endtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    time_spent = db.Column(db.Interval)
+    rating = db.Column(db.Numeric(2,1), default = 0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+'''
+DB for article sets that were seen by users (once a user rates an article the set he selected it from is added)
+'''
+class ArticleSetsSeen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
