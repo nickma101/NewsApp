9772f1cff395690dbed947e1c0742b628be39a7b
nickma101 <nimima95@gmail.com>
2022-10-10 12:10:12 +0200

    minor layout changes

--- frontend/src/components/Nudges/SelfActualisationNudgeDesktop.js
+++ frontend/src/components/Nudges/SelfActualisationNudgeDesktop.js
@@ -2,8 +2,7 @@ import React from "react";
 
 const SelfActualisationNudgeDesktop = {
   color: "red",
-  content:
-    "Door dit artikel te lezen zult u een beter geïnformeerde burger worden",
+  content: "Door dit artikel te lezen wordt u een beter geïnformeerde burger.",
   icon: "newspaper",
   ribbon: true,
   size: "huge",
